c05ed9c843a4f40c4496c5a619f913ef043e438e
TimSintegra <shabalintimon@ya.ru>
2026-04-26 17:42:14 +0300

    Настроить раздачу статики для админки

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,8 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = []
+STATIC_SOURCE_DIR = BASE_DIR / "static"
+if STATIC_SOURCE_DIR.exists():
+    STATICFILES_DIRS.append(STATIC_SOURCE_DIR)
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
